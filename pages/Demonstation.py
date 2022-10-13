1ca04453735a2b411adb3a85aedf6660e7bf11b8
Pratik Shrikant Rao <88026528+Ironspy06@users.noreply.github.com>
2022-10-13 21:10:06 +0530

    Update Demonstation.py

--- pages/Demonstation.py
+++ pages/Demonstation.py
@@ -1,22 +1,22 @@
 import streamlit as st
 import cv2
 import numpy as np
-import pyrebase
+# import pyrebase
 
-config = {
-  "apiKey": "AIzaSyCjULZ1FxzQHvMji5OR-OZnyOxY9KwV_GA",
-  "authDomain": "robot-68d4d.firebaseapp.com",
-  "databaseURL": "https://robot-68d4d-default-rtdb.firebaseio.com",
-  "projectId": "robot-68d4d",
-  "storageBucket": "robot-68d4d.appspot.com",
-  "messagingSenderId": "1071151697658",
-  "appId": "1:1071151697658:web:635787e41378b95fa54343",
-  "measurementId": "G-42522D1CWQ"
-}
+# config = {
+#   "apiKey": "AIzaSyCjULZ1FxzQHvMji5OR-OZnyOxY9KwV_GA",
+#   "authDomain": "robot-68d4d.firebaseapp.com",
+#   "databaseURL": "https://robot-68d4d-default-rtdb.firebaseio.com",
+#   "projectId": "robot-68d4d",
+#   "storageBucket": "robot-68d4d.appspot.com",
+#   "messagingSenderId": "1071151697658",
+#   "appId": "1:1071151697658:web:635787e41378b95fa54343",
+#   "measurementId": "G-42522D1CWQ"
+# }
 
-firebase=pyrebase.initialize_app(config)
+# firebase=pyrebase.initialize_app(config)
 
-db=firebase.database()
+# db=firebase.database()
 
 
 
@@ -37,20 +37,20 @@ video_bytes = video_file.read()
 
 st.video(video_bytes)
 
-st.subheader("CHANGE MOVEMENT")
+# st.subheader("CHANGE MOVEMENT")
 
-def snake():
+# def snake():
    
-   data={"Model":"Snake"}
-   print("hi")
-   db.child("Motion").update(data)
-def random():
-   data={"Model":"Random"}
-   print("hilo")
-   db.child("Motion").update(data)
+#    data={"Model":"Snake"}
+#    print("hi")
+#    db.child("Motion").update(data)
+# def random():
+#    data={"Model":"Random"}
+#    print("hilo")
+#    db.child("Motion").update(data)
 
-s=st.button(label='Snake Movement',on_click=snake)
-r=st.button(label='Random Movement',on_click=random)
+# s=st.button(label='Snake Movement',on_click=snake)
+# r=st.button(label='Random Movement',on_click=random)
 
 st.subheader("CAMERA INPUT")
 
