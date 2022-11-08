c38deead8fcd9a8b9b6b084cf7d23666ca515091
Pratik Shrikant Rao <88026528+Ironspy06@users.noreply.github.com>
2022-11-08 11:54:31 +0530

    Update Sensor_Data.py

--- pages/Sensor_Data.py
+++ pages/Sensor_Data.py
@@ -1,24 +1,7 @@
 from gc import isenabled
 import pyrebase
-import random
-import pandas as pd
 import streamlit as st
-import numpy as np
-import time
-import threading
-import streamlit.components.v1 as components
 import base64
-from PIL import Image
-# config= {
-#   "apiKey": "AIzaSyBHiBLqMM4j7O1QsCsbt-kQYNCfduhPRSI",
-#   "authDomain": "ultrasonic-ec709.firebaseapp.com",
-#   "databaseURL": "https://ultrasonic-ec709-default-rtdb.firebaseio.com",
-#   "projectId": "ultrasonic-ec709",
-#   "storageBucket": "ultrasonic-ec709.appspot.com",
-#   "messagingSenderId": "745166098162",
-#   "appId": "1:745166098162:web:5901af8a8772cf3dd78a6b",
-#   "measurementId": "G-CEFJHZJL05"
-# }
 
 config = {
   "apiKey": "AIzaSyCjULZ1FxzQHvMji5OR-OZnyOxY9KwV_GA",
@@ -35,8 +18,6 @@ firebase=pyrebase.initialize_app(config)
 
 db=firebase.database()
 
-# [theme]
-# base="dark"
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
@@ -61,22 +42,7 @@ def set_background(png_file):
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-#set_background("C:/Users/Pratik/OneDrive/Desktop/Project/robot.png")
 set_background("C:/Users/Pratik/OneDrive/Desktop/Project/My project.jpg")
-
-# st.title("SENSOR DATA")
-# def snake():
-   
-#    data={"Model":"Snake"}
-#    print("hi")
-#    db.child("Motion").update(data)
-# def random():
-#    data={"Model":"Random"}
-#    print("hilo")
-#    db.child("Motion").update(data)
-
-# s=st.button(label='Snake Movement',on_click=snake)
-# r=st.button(label='Random Movement',on_click=random)
 
 
 res=-1
